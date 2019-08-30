5f0519f689af81751b91e60ed4051fc44726d216
TBB000623 <tbb000623@gmail.com>
2019-08-31 00:50:46 +0800

    v2.4.2 增加gcd模块(采用常数更友好的更相减损术)

--- LMath.h
+++ LMath.h
@@ -1,25 +1,13 @@
-#ifndef TBBLMAT_H
+#ifndef TBBLMAT_H //LMath.h ver:2.4.2
 #define TBBLMAT_H
 
-#include "D:\LongInt\LongInt\LInt.h"
+#include "LInt.h"
 namespace tbb {
     LInt abs(const LInt &B)	{
 		LInt ans(B);
 		if (B.sign<0) ans.sign=-B.sign;
 		return ans;
 	};
-    LInt mul_pow10(const LInt &A, int k)    {
-        if(A.isNaN()||A.zero()||A.isinf())  return A;
-        int t1;
-        switch(k%4) {
-            case(0):    t1= 1;  break;
-            case(1):    t1= 10; break;
-            case(2):    t1= 100;break;
-            default:    t1=1000;break;
-        }
-        return (A* t1)<<(k/4);
-    }
-    LInt pow10(int k)   {return mul_pow10(1,k);}
     //get 10000^2k/sqrt(A) when A has 2k or 2k-1 bits
     LInt recip_2(const LInt &A) {
         if(A.isNaN())   return false;
@@ -27,7 +15,7 @@ namespace tbb {
             if(A.d==0)	return 0;
             u32 temp;
             if(A.d==1)	temp=A.num[0];
-            if(A.d==2)	temp=A.num[0]+A.num[1]*10000;
+            else    	temp=A.num[0]+A.num[1]*10000;
             return int(pow(10000,A.d)/std::sqrt(double(temp)));
         }
         int k= (A.d+1)/2;
@@ -69,43 +57,7 @@ namespace tbb {
 		}
 		return ans;
 	}
-/*    LInt pow(const LInt &A, u32 k)    {
-        using std::vector;
-        using std::complex;
-        using std::cerr;
-        if(A==0&&k==0)  {ERROR(3); return LInt(false);}
-        if(A==0)    return LInt(0);
-        if(k==0||A==1)    return LInt(1);
-        if(A.num[0]==0) {
-            int lnz=0;  //lnz=the lower bit where is not 0
-            for(lnz=0; lnz<A.d&&A.num[lnz]==0; lnz++);
-            return pow(A>>lnz,k)<<(lnz*k);
-        }
-        LInt ans;
-        u32 head= A.num[A.d-1]; if(A.d!=1)  head++;
-        int N=1<<Log_2(int(ceil(k* (log10(head)/4+ A.d-1))));
-        ans.d=N+1;    ans.sign=(k%2)?A.sign:1;
-        ans.num= new u32[ans.d];    memset(ans.num, 0, ans.d*sizeof(u32));
-        vector< complex<double> > pI(N,0),pO,aI,aO(N,0);
-        for(int i=0; i<A.d; i++)  pI[i]= A.num[i];
-        aI=FFT(pI, false);
-        for(int i=0; i<N; i++)  aO[i]=pow(aI[i], k);
-        pO=FFT(aO, true);
-        cerr<<pO[0]<<endl<<pO[N-1]<<endl;
-        double carry=0.0;
-        for(int i=0; i<N; i++)	{
-            double temp=round(real(pO[i])+carry);
-            carry=round(temp/10000);
-            int base=(int)round(temp-carry*10000);
-            if(base<0)	base+=10000,carry-=1.0;
-            ans.num[i]=base;
-        }
-        ans.num[N]=(unsigned)carry;
-        ans.sho();
-        return ans;
-    }   */
     LInt pow(const LInt &A, int k)  {
-        using std::cerr;
         if((A==0&&k==0)||A.isNaN())  return false;
         if(A.isinf()&&k>=0)   {
             if(k==0)    return false;
@@ -126,7 +78,7 @@ namespace tbb {
     LInt powrt(const LInt &A, int k)  {
         if(A.isNaN()||k<=0)   return false;
         if(A.zero())    return 0;
-        if(A.ngtive()&&k%2==0)  return false;
+        if(A.negative()&&k%2==0)  return false;
         if(A.isinf()) return A;
         LInt x0= pow10(4*(A.d-1)/k), x;
         while(true) {
@@ -152,6 +104,24 @@ namespace tbb {
             step/=2;
         }
         return x;
+    }
+    LInt gcd(LInt a, LInt b)  {
+        if(a.isNaN()||b.isNaN())    return false;
+        LInt pow= 1;
+		int div= 0;
+        a= abs(a);  b= abs(b);
+        while(!b.zero())    {
+            if(a<b)   {swap(a,b); div= 0; continue;}
+			if(div==0)	div= (a.d>b.d+1)?1:-1;
+            if(a[0]%2==0&&b[0]%2==0)    {a= a.div2();    b= b.div2();    pow*=2; continue;}
+            if(a[0]%2==0)   {a= a.div2();   continue;}
+            if(b[0]%2==0)   {b= b.div2();   continue;}
+            if(div==1)	a%=b;	else	a-=b;
+        }
+        return a* pow;
+    }
+    LInt lcd(const LInt &a, const LInt &b)  {
+        return (a*b)/gcd(a,b);
     }
 }
 #endif
